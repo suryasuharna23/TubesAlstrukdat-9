c9ca9252ed2270dd480d19942b0eba3e387b05fe
naurchmn <naurchmn@gmail.com>
2024-11-18 20:12:38 +0700

    add : header ADT barang

---
+++ src/ADT/Barang/barang.h
@@ -0,0 +1,29 @@
+#ifndef BARANG_H
+#define BARANG_H
+
+#define MAX_LEN 100 // Panjang maksimal nama barang
+
+typedef struct {
+    char name[MAX_LEN];  // Nama barang
+    int price;           // Harga barang
+} Barang;
+
+typedef struct {
+    Barang *buffer;    // Array dinamis barang
+    int capacity;      // Kapasitas maksimum
+    int size;          // Jumlah elemen saat ini
+} ListDinamicBarang;
+
+Barang CreateBarang (const char *name, int price); // membuat barang baru
+
+void PrintBarang (const Barang *barang); // mencetak informasi barang
+
+
+
+
+
+
+
+
+
+#endif
--- src/ADT/User/driveruser.c
+++ src/ADT/User/driveruser.c
@@ -8,11 +8,13 @@ int main() {
     User user2;
     User user3;
     CreateUser(&user1, "surya", "securepass", 500);
-    CreateUser(&user2, "suharna", "securepass", 500);
-    CreateUser(&user3, "surya suharna", "securepass", 500);
+    CreateUser(&user2, "suharna", "securepass", 400);
+    CreateUser(&user3, "surya suharna", "securepassssssssssssssssssssssssssssssssssssssssssssssssssssss", 100);
 
     printf("User created successfully:\n");
-    PrintUser(&user2); //ngecek
+    PrintUser(&user1); //ngecek
+    PrintUser(&user2); 
+    PrintUser(&user3); 
 
     return 0;
 }
