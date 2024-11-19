7fbaf03a24e0a76404aa6115db100c054fe91cfc
naurchmn <naurchmn@gmail.com>
2024-11-19 18:53:40 +0700

    add: boolean.h

--- src/ADT/Barang/barang.h
+++ src/ADT/Barang/barang.h
@@ -1,6 +1,8 @@
 #ifndef BARANG_H
 #define BARANG_H
 
+#include "boolean.h"
+
 #define MAX_LEN 100 // Panjang maksimal nama barang
 
 typedef struct {
@@ -19,12 +21,6 @@ Barang CreateBarang (const char *name, int price); // membuat barang baru
 void CreateListDinamicBarang(ListDinamicBarang *list, int capacity); //alokasi list barang
 
 void PrintBarang (const Barang *barang); // mencetak informasi barang
-
-
-
-
-
-
 
 
 
