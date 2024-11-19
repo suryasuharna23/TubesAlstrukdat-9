47498227f27137fb2a3f8a8c5e0c6a3f774c203f
Surya Suharna <166743679+suryasuharna23@users.noreply.github.com>
2024-11-19 09:40:05 +0700

    coba-coba

--- src/ADT/Barang/barang.c
+++ src/ADT/Barang/barang.c
@@ -29,3 +29,106 @@ void PrintBarang (const Barang *barang) { // Mencetak informasi barang
 }
 
 
+/*========== KONSTRUKTOR ==========*/
+/**
+ * Destruktor
+ * I.S. ArrayDin terdefinisi
+ * F.S. array->store terdealokasi
+ */
+void DeallocateArrayDin(ArrayDinStore *array){
+    free((*array).store);
+    (*array).Capacity = 0;
+    (*array).Neff=0;
+}
+
+/**
+ * Fungsi untuk mengetahui apakah suatu array kosong.
+ * Prekondisi: array terdefinisi
+ */
+boolean IsEmptyArrayDin(ArrayDinStore array){
+    return (array.Neff == 0);
+}
+
+/**
+ * Fungsi untuk mendapatkan banyaknya elemen efektif array, 0 jika tabel kosong.
+ * Prekondisi: array terdefinisi
+ */
+int Length(ArrayDinStore array){
+    return array.Neff;
+}
+
+/**
+ * Mengembalikan elemen array L yang ke-I (indeks lojik).
+ * Prekondisi: array tidak kosong, i di antara 0..Length(array).
+ */
+address Get(ArrayDinStore array, int i){
+    return (array.store[i]).First;
+}
+
+/**
+ * Fungsi untuk mendapatkan kapasitas yang tersedia.
+ * Prekondisi: array terdefinisi
+ */
+int GetCapacity(ArrayDinStore array){
+    return array.Capacity;
+}
+
+/**
+ * Fungsi untuk menambahkan elemen baru di index ke-i
+ * Prekondisi: array terdefinisi, i di antara 0..Length(array).
+ */
+void InsertAt(ArrayDinStore *array, address P, int i){
+    int j;
+
+    for (j = array->Neff - 1; j >= i; j--){
+        array->store[j+1] = array->store[j];
+    }
+    
+    array->store[i].First = P;
+    array->Neff += 1;
+}
+
+/**
+ * Fungsi untuk menambahkan elemen baru di akhir array.
+ * Prekondisi: array terdefinisi
+ */
+void InsertLast(ArrayDinStore *array, address el){
+    InsertAt(array, el, Length(*array));
+}
+
+/**
+ * Fungsi untuk menambahkan elemen baru di awal array.
+ * Prekondisi: array terdefinisi
+ */
+void InsertFirst(ArrayDinStore *array, address el){
+    InsertAt(array, el, 0);
+}
+
+/**
+ * Fungsi untuk menghapus elemen di index ke-i ArrayDin
+ * Prekondisi: array terdefinisi, i di antara 0..Length(array).
+ */
+void DeleteAt(ArrayDinStore *array, int i){
+    
+    int j;
+    for (j=i;j<array->Neff; j++){
+        array->store[j] = array->store[j+1];
+    }
+    array->Neff -= 1;
+}
+
+/**
+ * Fungsi untuk menghapus elemen terakhir ArrayDin
+ * Prekondisi: array tidak kosong
+ */
+void DeleteLast(ArrayDinStore *array){
+    DeleteAt(array, Length(*array)-1);
+}
+
+/**
+ * Fungsi untuk menghapus elemen pertama ArrayDin
+ * Prekondisi: array tidak kosong
+ */
+void DeleteFirst(ArrayDinStore *array){
+    DeleteAt(array, 0);
+}
