7de5690f9616cff5c70a07b9c1d3085869a67446
naurchmn <naurchmn@gmail.com>
2024-11-24 22:00:31 +0700

    add load & edit save

--- src/command/Save/save.c
+++ src/command/Save/save.c
@@ -1,2 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "save.h"
+
+void save(const char *filename, ArrayDinUser  *userArray, listBarang *barangList) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Gagal membuka file.\n");
+        return;
+    }
+
+    // nyimpen info pengguna
+    fprintf(file, "Users:\n");
+    for (int i = 0; i < userArray->Neff; i++) {
+        fprintf(file, "Username: %s\n", userArray->users[i].name);
+        fprintf(file, "Password: %s\n", userArray->users[i].password);
+        fprintf(file, "Uang: %d\n", userArray->users[i].money);
+    }
+
+    // nyimpen info barang
+    fprintf(file, "Barang:\n");
+    for (int i = 0; i < barangList->count; i++) {
+        fprintf(file, "Nama: %s\n", barangList->items[i].name);
+        fprintf(file, "Harga: %d\n", barangList->items[i].price);
+    }
+
+    fclose(file);
+    printf("Save file berhasil disimpan.\n");
+}
--- src/command/Save/save.h
+++ src/command/Save/save.h
@@ -1,4 +1,14 @@
-#ifndef __SAVE_H__
-#define __SAVE_H__
+#ifndef SAVE_H
+#define SAVE_H
+
+#include "../../ADT/Mesin/mesinkarakter.h"
+#include "../../ADT/Mesin/mesinkata.h"
+#include "../../ADT/Barang/barang.h"
+#include "../../ADT/User/user.h"
+
+#define MAX_USER_COUNT 100
+#define MAX_BARANG_COUNT 100
+
+void save(const char *filename, ArrayDinUser  *userArray, listBarang *barangList);
 
 #endif
