7244d1c36eff35bff7adab2872ff13ae5a0d22c0
Nawaf Amjad Rizqi Aldaha Ismail <19623217@std.stei.itb.ac.id>
2024-11-24 22:05:46 +0700

    wordl3, tebak_angka

--- src/command/Tebak_Angka/tebak_angka.c
+++ src/command/Tebak_Angka/tebak_angka.c
@@ -8,30 +8,31 @@ int getInt(){
     STARTINPUTWORD();
     Word num = CurrentWord;
 
-    return WordToInt(num);
+    return WordToInt(num); // mengubah word menjadi integer
 }
 
 void tebak_angka(){
     int try = MAX_TRIES;
 
-    int digit = (randomNumber(3) % 3) + 1;
-    int key_value = randomNumber(3) % (int)(pow(10, digit));
+    int digit = (randomNumber(3) % 3) + 1; // menentukan banyaknya digit angka akan ditebak
+    int key_value = randomNumber(3) % (int)(pow(10, digit)); // merandom bilangan yang akan ditebak
 
     boolean win = false;
 
     while (try > 0 && !win){
         int value;
-
+        
+        // menerima masukan angka tebakam
         printf("Tebak angka: ");
         value = getInt();
 
-        if (value < key_value){
+        if (value < key_value){ // kalau nilai yang ditebak lebih kecil dari nilai sebenarnya
             printf("Tebakanmu lebih kecil!\n");
             printf("(kesempatanmu %d kali lagi untuk mencoba)\n", (try-1));
-        } else if (value > key_value){
+        } else if (value > key_value){ // kalau nilai yang ditebak lebih besar dari nilai sebenarnya
             printf("Tebakanmu lebih besar!\n");
             printf("(kesempatanmu %d kali lagi untuk mencoba)\n", (try-1));
-        } else {
+        } else { // kalau nilai yang ditebak sama dengan nilai sebenarnya
             printf("Tebakanmu benar!");
             int poin = score(75, try);
             printf(" +%d rupiah telah ditambahkan ke akun anda.\n", poin);
@@ -40,7 +41,7 @@ void tebak_angka(){
         try--;
     }
 
-    if (!win){
+    if (!win){ // kalau kalah
         printf("Yahh kamu kalah \\(>A<)/\n");
         printf("Angkamu yang benar adalah %d", key_value);
     }
