1b733d79c9ab059248cd559b634fb404d418eae7
Persada Ramiiza Abyudaya <persadaramiiza@gmail.com>
2024-12-19 00:04:45 +0700

    checkpoint save

--- src/command/Work/work.c
+++ src/command/Work/work.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "work.h"
 
-typedef struct {
+typedef struct
+{
     char jobName[50];
     int salary;
-    int duration; 
+    int duration;
 } Job;
 
 Job jobs[] = {
@@ -12,65 +13,73 @@ Job jobs[] = {
     {"OWCA Hiring Manager", 4200, 21},
     {"Cikapundunginator Caretaker", 7000, 30},
     {"Mewing Specialist", 10000, 22},
-    {"Inator Connoisseur", 997, 15}
-};
+    {"Inator Connoisseur", 997, 15}};
 
 int jobCount = sizeof(jobs) / sizeof(jobs[0]);
 
 // cocokin nama pekerjaan
-boolean compareJobName(Word input, char *jobName) {
+boolean compareJobName(Word input, char *jobName)
+{
     int i;
     int jobNameLength = 0;
-    while (jobName[jobNameLength] != '\0') {
+    while (jobName[jobNameLength] != '\0')
+    {
         jobNameLength++;
     }
 
-    if (input.Length != jobNameLength) {
+    if (input.Length != jobNameLength)
+    {
         return false;
     }
 
-    for (i = 0; i < input.Length; i++) {
-        if (input.TabWord[i] != jobName[i]) {
-            return false; 
+    for (i = 0; i < input.Length; i++)
+    {
+        if (input.TabWord[i] != jobName[i])
+        {
+            return false;
         }
     }
     return true;
 }
 
-
-void Work(User *user) {
+void Work(User *user)
+{
     printf("User %s now has %s as password.\n", user->name, user->password);
     printf("User %s now has %d money.\n", user->name, user->money);
     printf("Daftar pekerjaan:\n");
     printf("+----+-----------------------------+-----------+---------+\n");
     printf("| No | Job Name                    | Salary    | Duration|\n");
     printf("+----+-----------------------------+-----------+---------+\n");
-    for (int i = 0; i < jobCount; i++) {
+    for (int i = 0; i < jobCount; i++)
+    {
         printf("| %-2d | %-27s | %-9d | %-7ds |\n", i + 1, jobs[i].jobName, jobs[i].salary, jobs[i].duration);
     }
     printf("+----+-----------------------------+-----------+---------+\n");
 
     printf("\nMasukkan nama pekerjaan yang dipilih: \n");
     printf(">>> ");
-    STARTINPUTWORD(); 
+    STARTINPUTWORD();
 
     Job *selectedJob = NULL; // pointer ke pekerjaan yang dipilih
-    for (int i = 0; i < jobCount; i++) {
-        if (compareJobName(CurrentWord, jobs[i].jobName)) {
+    for (int i = 0; i < jobCount; i++)
+    {
+        if (compareJobName(CurrentWord, jobs[i].jobName))
+        {
             selectedJob = &jobs[i];
             break;
         }
     }
 
-    if (selectedJob == NULL) {
+    if (selectedJob == NULL)
+    {
         printf("Pilihan pekerjaan tidak valid!\n");
         return;
     }
 
-    printf("\nAnda sedang bekerja sebagai %s... harap tunggu selama %d detik.\n", 
-            selectedJob->jobName, selectedJob->duration);
+    printf("\nAnda sedang bekerja sebagai %s... harap tunggu selama %d detik.\n",
+           selectedJob->jobName, selectedJob->duration);
     displayLoadingScreen(selectedJob->duration);
-    user->money += selectedJob->salary; 
+    user->money += selectedJob->salary;
     printf("Pekerjaan selesai, +%d rupiah telah ditambahkan ke akun Anda.\n", selectedJob->salary);
 
     printf("User %s now has %d money.\n", user->name, user->money);
